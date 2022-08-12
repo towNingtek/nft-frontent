ffb5cf1e6fe00225a5d01f5eb8311847b43a3b4c
yillkid <yillkid@gmail.com>
2022-08-12 15:50:36 +0800

    feat: implement connect wallet

--- docs/static/js/function.js
+++ docs/static/js/function.js
@@ -2265,9 +2265,11 @@ const payABI = `
 document.getElementById("connect").onclick = function() {
     town.connectMetaMask();
 }
+/*
 document.getElementById("purchase").onclick = function() {
     town.purchase("user1_abc", 1, "0x5faF6582Ede7D65362dcF343B42b83B2F3E2E7CF", abiC, "20");
 }
 document.getElementById("remainder").onclick = function() {
     town.getRemainder();
 }
+*/
